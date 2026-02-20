f1c0da6ec97715164dbe8d4e98e2447ece9028af
Vish <kelvishvargas23@gmail.com>
2026-02-20 14:29:34 +0800

    Added bullet and custom Group class implementation

---
+++ bullet.py
@@ -0,0 +1,60 @@
+import pygame, math, random
+from configs import *
+from game_map import tiles
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, image, x, y, scroll, direction, mouse_target_x, mouse_target_y):
+        super().__init__()
+        self.image = pygame.transform.scale(pygame.image.load(image), (BULLET_SIZE, BULLET_SIZE))
+        self.rect = self.image.get_rect(topleft=(x, y))
+        self.speed = 450
+        self.direction = direction
+        self.pos = pygame.Vector2(x, y)
+        self._mouse_target_x = mouse_target_x + scroll[0]
+        self._mouse_target_y = mouse_target_y + scroll[1]
+        self._angle = math.atan2(self._mouse_target_y - self.pos.x, self._mouse_target_x - self.pos.y)
+        self._x_vel = math.cos(self._angle)*self.speed
+        self._y_vel = math.sin(self._angle)*self.speed
+
+        self.particles = []
+    
+    def update(self, dt, scroll):
+        ########## TRAIL PARTICLE ##########
+        for particle in self.particles:
+            particle[0][0] -= 1
+            particle[0][1] += particle[1]
+        particle = [list(self.rect.midleft), random.uniform(-2, 2), pygame.Color(255, random.randrange(255), 0)]
+        self.particles.append(particle)
+        if len(self.particles) > 20:
+            self.particles.pop(0)
+        ########## TRAIL PARTICLE ##########
+        self._draw_particles(scroll)
+
+        if self.rect.x > WINDOW_WIDTH:
+            self.kill()
+        elif self.rect.x < 0:
+            self.kill()
+        
+        if self.direction > 0:
+            self.speed = self.speed
+        elif self.direction < 0:
+            self.speed = -400
+
+        self._kill_if_tile_collision()
+
+        self._move(dt)
+
+    def _move(self, dt):
+        self.pos.x += self._x_vel * dt
+        self.pos.y += self._y_vel * dt
+        self.rect.x = int(self.pos.x)
+        self.rect.y = int(self.pos.y)
+
+    def _draw_particles(self, scroll):
+        for i, particle in enumerate(self.particles):
+            pygame.draw.circle(display, particle[2], (particle[0][0]-scroll[0], particle[0][1]-scroll[1]), (i//3+2))
+
+    def _kill_if_tile_collision(self):
+        for tile in tiles:
+            if self.rect.colliderect(tile.rect):
+                self.kill()
